cd43c6f7efac673ecf89c1ff39754fe76afe3318
Thomas Leese <inbox@thomasleese.me>
2015-11-23 09:15:31 +0000

    Change size of the window

--- desktop/src/org/suhps/simulation/desktop/DesktopLauncher.java
+++ desktop/src/org/suhps/simulation/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import org.suhps.simulation.SubmarineSimulation;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1024;
+		config.width = 1280;
 		config.height = 768;
 		config.useHDPI = true;
 		config.vSyncEnabled = true;
